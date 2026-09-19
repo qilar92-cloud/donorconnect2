cb1a6c1f01eaa94c5d9f3b9e862449f385a3d1d6
Aqilah Dwi Ramadhani <qilar92@gmail.com>
2026-09-19 20:54:24 +0700

    Perbaiki tampilan edit profil pendonor

--- resources/views/pages/pendonor/profile/edit.blade.php
+++ resources/views/pages/pendonor/profile/edit.blade.php
@@ -10,50 +10,23 @@
 
 <div class="edit-page">
 
-    {{-- Judul --}}
-    <div class="page-title">
-
-        <h1>Ubah Profil</h1>
-
-        @if(isset($pendonor))
-
-            <p>
-                Perbarui informasi profil pendonor.
-            </p>
-
-        @elseif(isset($petugas))
-
-            <p>
-                Perbarui informasi profil petugas PMR.
-            </p>
-
-        @endif
-
-    </div>
-
-
     {{-- Error --}}
     @if($errors->any())
 
         <div class="alert alert-danger">
-
             <i class="fas fa-exclamation-circle"></i>
 
             <ul>
-
                 @foreach($errors->all() as $error)
-
                     <li>{{ $error }}</li>
-
                 @endforeach
-
             </ul>
-
         </div>
 
     @endif
 
 
+    {{-- Card --}}
     <div class="edit-card">
 
         {{-- Header --}}
@@ -64,23 +37,13 @@
             </div>
 
             <div>
-
                 <h2>Ubah Profil</h2>
 
                 @if(isset($pendonor))
-
-                    <span>
-                        Perbarui data pribadi pendonor.
-                    </span>
-
+                    <span>Perbarui data pribadi pendonor.</span>
                 @elseif(isset($petugas))
-
-                    <span>
-                        Perbarui data akun petugas PMR.
-                    </span>
-
+                    <span>Perbarui data akun petugas PMR.</span>
                 @endif
-
             </div>
 
         </div>
@@ -103,46 +66,32 @@
                 {{-- Pendonor --}}
                 @if(isset($pendonor))
 
-                    {{-- Nama --}}
                     <div class="form-row">
-
-                        <label>
-                            Nama Lengkap
-                        </label>
+                        <label>Nama Lengkap</label>
 
                         <input
                             type="text"
                             name="nama"
-                            value="{{ old('nama', $user->nama ?? $pendonor->user->nama ?? '') }}"
+                            value="{{ old('nama', $user->nama ?? '') }}"
                             required
                         >
-
                     </div>
 
 
-                    {{-- Email --}}
                     <div class="form-row">
-
-                        <label>
-                            Email
-                        </label>
+                        <label>Email</label>
 
                         <input
                             type="email"
                             name="email"
-                            value="{{ old('email', $user->email ?? $pendonor->user->email ?? '') }}"
+                            value="{{ old('email', $user->email ?? '') }}"
                             required
                         >
-
                     </div>
 
 
-                    {{-- Status --}}
                     <div class="form-row">
-
-                        <label>
-                            Status
-                        </label>
+                        <label>Status</label>
 
                         <input
                             type="text"
@@ -150,16 +99,11 @@
                             value="{{ old('status', $pendonor->status ?? '') }}"
                             required
                         >
-
                     </div>
 
 
-                    {{-- Kelas / Jabatan --}}
                     <div class="form-row">
-
-                        <label>
-                            Kelas / Jabatan
-                        </label>
+                        <label>Kelas / Jabatan</label>
 
                         <input
                             type="text"
@@ -167,16 +111,11 @@
                             value="{{ old('kelas_jabatan', $pendonor->kelas_jabatan ?? '') }}"
                             required
                         >
-
                     </div>
 
 
-                    {{-- Tanggal Lahir --}}
                     <div class="form-row">
-
-                        <label>
-                            Tanggal Lahir
-                        </label>
+                        <label>Tanggal Lahir</label>
 
                         <input
                             type="date"
@@ -189,16 +128,11 @@
                             ) }}"
                             required
                         >
-
                     </div>
 
 
-                    {{-- Golongan Darah --}}
                     <div class="form-row">
-
-                        <label>
-                            Golongan Darah
-                        </label>
+                        <label>Golongan Darah</label>
 
                         <input
                             type="text"
@@ -206,16 +140,11 @@
                             value="{{ old('golongan_darah', $pendonor->golongan_darah ?? '') }}"
                             required
                         >
-
                     </div>
 
 
-                    {{-- No. Telepon --}}
                     <div class="form-row">
-
-                        <label>
-                            No. Telepon
-                        </label>
+                        <label>No. Telepon</label>
 
                         <input
                             type="text"
@@ -223,16 +152,11 @@
                             value="{{ old('nomor_telepon', $pendonor->nomor_telepon ?? '') }}"
                             required
                         >
-
                     </div>
 
 
-                    {{-- Informasi Kesehatan --}}
                     <div class="form-row">
-
-                        <label>
-                            Informasi Kesehatan
-                        </label>
+                        <label>Informasi Kesehatan</label>
 
                         <input
                             type="text"
@@ -240,44 +164,33 @@
                             value="{{ old('informasi_kesehatan', $pendonor->informasi_kesehatan ?? '') }}"
                             required
                         >
-
                     </div>
 
 
                 {{-- Petugas --}}
                 @elseif(isset($petugas))
 
-                    {{-- Nama --}}
                     <div class="form-row">
-
-                        <label>
-                            Nama Lengkap
-                        </label>
+                        <label>Nama Lengkap</label>
 
                         <input
                             type="text"
                             name="nama"
-                            value="{{ old('nama', $user->nama ?? $petugas->user->nama ?? '') }}"
+                            value="{{ old('nama', $user->nama ?? '') }}"
                             required
                         >
-
                     </div>
 
 
-                    {{-- Email --}}
                     <div class="form-row">
-
-                        <label>
-                            Email
-                        </label>
+                        <label>Email</label>
 
                         <input
                             type="email"
                             name="email"
-                            value="{{ old('email', $user->email ?? $petugas->user->email ?? '') }}"
+                            value="{{ old('email', $user->email ?? '') }}"
                             required
                         >
-
                     </div>
 
                 @endif
@@ -292,13 +205,9 @@
                     type="submit"
                     class="save-button"
                 >
-
                     <i class="fas fa-save"></i>
-
                     Simpan Perubahan
-
                 </button>
-
 
                 <a
                     href="{{ isset($pendonor)
@@ -306,9 +215,7 @@
                         : route('profile.petugas') }}"
                     class="cancel-button"
                 >
-
                     Batal
-
                 </a>
 
             </div>
